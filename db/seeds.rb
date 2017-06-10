f79a5916e8ac7a3b064c4e7bbdc7f142810f7e50
Diego Rosario Brogna <me@dierbro.com>
2017-06-10 08:22:06 +0200

    Create campsite, activities and children to seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Campsite
+campsite = Campsite.create(name: "FunCamp")
+
+# Create Activities
+activities = %w/A B C/.map{ |l| campsite.activities.create(name: "Activity#{l}") }
+
+# Create children for each activity
+activities.each do |a|
+  rand(6..10).times{ a.children.create(name: Faker::Name.name_with_middle) }
+end
