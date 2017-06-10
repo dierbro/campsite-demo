328571455605d54b9fada49fb5aed363ef5b20ae
Diego Rosario Brogna <me@dierbro.com>
2017-06-10 08:25:21 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  resources :campsites, only: [] do
+    resources :activities, only: [:index] do
+      resources :children, only: [:index, :update], shallow: true
+    end
+  end
 end
